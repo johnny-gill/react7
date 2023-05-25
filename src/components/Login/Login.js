4c74342fd15adc81f7a4e16d46225b66e09a9bae
johnny-gill <uint777@gmail.com>
2023-05-25 15:51:22 +0900

    login state 관리 컴포넌트 분리

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
 
 const emailReducer = (state, action) => {
   console.log('emailReducer : ', action.type);
@@ -45,6 +46,8 @@ const Login = (props) => {
     isValid: false,
   });
 
+  const authCtx = useContext(AuthContext);
+
   useEffect(() => {
     console.log('EFFECT RUNNING');
 
@@ -86,7 +89,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
